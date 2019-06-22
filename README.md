7e1c73eb25c5843d15d82d428c82a9341d481f79
Reymundo Tenorio <reymundotenorio@gmail.com>
2019-06-22 11:18:33 -0700

    Adding header to React element

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ReactTallerDrupal
+
+## React
+## React DOM
+## Babel
+## JSX
--- script.js
+++ script.js
@@ -7,8 +7,13 @@
 // );
 
 const elemento = (
-  <p className="may" style={{ color: "red" }}>
-    El mejor <strong>cine al aire</strong> libre de Managua.
-  </p>
+  <header>
+    <h1>Cine de las estrellas</h1>
+
+    <p className="may" style={{ color: "red" }}>
+      El mejor <strong>cine al aire</strong> libre de Managua.
+    </p>
+  </header>
 );
+
 ReactDOM.render(elemento, document.getElementById("app"));
