c8eca3182c8eb38a56e7eb40598fb7e9243ea780
Reymundo Tenorio <reymundotenorio@gmail.com>
2019-06-22 12:58:57 -0700

    Adding seed map

--- script.js
+++ script.js
@@ -1,19 +1,82 @@
-// let elemento = React.createElement(
-//   "p",
-//   { className: "may" },
-//   "El mejor",
-//   React.createElement("strong", null, " cine al aire "),
-//   "libre de Managua."
-// );
+// Movies API
+const seed = [
+  {
+    id: "tt0117705",
+    name: "Space Jam",
+    year: 1996,
+    votes: 5,
+    cover:
+      "https://m.media-amazon.com/images/M/MV5BMDgyZTI2YmYtZmI4ZC00MzE0LWIxZWYtMWRlZWYxNjliNTJjXkEyXkFqcGdeQXVyNjY5NDU4NzI@._V1_SX300.jpg"
+  },
+  {
+    id: "tt0158811",
+    name: "Muppets from Space",
+    year: 1999,
+    votes: 3,
+    cover:
+      "https://m.media-amazon.com/images/M/MV5BODI2Zjc5YzMtNzQ1NS00NGVmLWExYzgtMzFlY2NiMGMzZjRjXkEyXkFqcGdeQXVyNTUyMzE4Mzg@._V1_SX300.jpg"
+  },
+  {
+    id: "tt0062622",
+    name: "2001: A Space Odyssey",
+    year: 1968,
+    votes: 1,
+    cover:
+      "https://m.media-amazon.com/images/M/MV5BMmNlYzRiNDctZWNhMi00MzI4LThkZTctMTUzMmZkMmFmNThmXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+  }
+];
 
-const elemento = (
-  <header>
-    <h1>Cine de las estrellas</h1>
+// Header component
+function Header() {
+  return (
+    <header>
+      <h1>Cine de las estrellas si</h1>
 
-    <p className="may" style={{ color: "red" }}>
-      El mejor <strong>cine al aire</strong> libre de Managua.
-    </p>
-  </header>
-);
+      <p className="may" style={{ color: "red" }}>
+        El mejor <strong>cine al aire</strong> libre de Managua.
+      </p>
+    </header>
+  );
+}
+// End Header component
 
-ReactDOM.render(elemento, document.getElementById("app"));
+// Movie component
+function Movie(props) {
+  return (
+    <article>
+      <h3>{props.nombre}</h3>
+      <div className="year">Publicada en {props.anio}</div>
+      <img alt="Portada de ${props.nombre}" src={props.foto} />
+    </article>
+  );
+}
+
+// Full page component
+function ContenedorApp() {
+  return (
+    // <>
+    <React.Fragment>
+      <Header />
+
+      <h2>En cartelera</h2>
+
+      <section>
+        {seed.map(item => {
+          // function (item)
+          return (
+            <Movie
+              key={item.id}
+              nombre={item.name}
+              anio={item.year}
+              foto={item.cover}
+            />
+          );
+        })}
+      </section>
+    </React.Fragment>
+    // </ >
+  );
+}
+
+// Render Header component
+ReactDOM.render(<ContenedorApp />, document.getElementById("app"));
